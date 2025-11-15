dbbc4e9d6983fc0b7a09f91b291d7e76991eded6
Alexandr Shagov <alexandr.shg01@yandex.ru>
2025-11-15 21:34:34 +0300

    develop/create-loggers

---
+++ src/test/java/FilmsControllerTest.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+
+@WebMvcTest(FilmController.class)
+public class FilmsControllerTest {
+
+
+}
